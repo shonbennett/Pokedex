3cc67709d91aa79b015413ccbf426ddc2593d5a8
Shon Bennett <acashonbennett@gmail.com>
2023-11-01 12:07:12 -0400

    Test API Call

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -6,19 +6,49 @@
 //
 
 import SwiftUI
+import PokemonAPI
 
 struct ContentView: View {
+    @State private var pokemonName: String? // Store the fetched Pokémon name
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            // Display the Pokémon name if it's available
+            if let name = pokemonName {
+                Text("Fetched Pokémon name: \(name)")
+            } else {
+                Text("Loading Pokémon name...")
+            }
         }
-        .padding()
+        .onAppear {
+            // Fetch the Pokémon name when the view appears
+            pokemonServiceCall { result in
+                switch result {
+                case .success(let name):
+                    // Update the @State variable to reflect the fetched name
+                    pokemonName = name
+                case .failure(let error):
+                    print("Error")
+                }
+            }
+        }
     }
 }
-
 #Preview {
     ContentView()
 }
+
+func pokemonServiceCall(completion: @escaping (Result<String, Error>) -> Void) {
+    // Example of calling a web API using a name
+    PokemonAPI().pokemonService.fetchPokemon("charizard") { result in
+        switch result {
+        case .success(let pokemon):
+            pokemon.name = pokemon.name // bulbasaur
+            completion(.success(pokemon.name ?? "empty"))
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+}
+
