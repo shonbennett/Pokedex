cc3edbc9aea2b93b38163020132c052202d4ae4b
Shon Bennett <acashonbennett@gmail.com>
2023-11-01 11:39:47 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Shon Bennett on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
